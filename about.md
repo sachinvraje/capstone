485b69a11d20145b5ff3589630c3f1efe4d38f7e
Sachin Raje <sachinvraje@gmail.com>
2017-02-13 01:22:22 +0400

    First Commit

--- about.md
+++ about.md
@@ -1,22 +1,22 @@
-###Dear Reader,
 
-This application is developed as part of the requirement for the **Coursera Data Science Capstone Course**. The goal of the project is to build a predictive text model combined with a shiny app UI that will predict the next word as the user types a sentence similar to the way most smart phone keyboards are implemented today using the technology of Swiftkey.
+#### Dear Reader,
+As part of the Data Science Specialisation course from Coursera, this Shiny app is developed for the Capstone project. The goal of this Shiny app is to build a predictive text model that will predict the next word as the user types a sentence. The app considers the last 3 words typed, and predicts the next word, based on the SwiftKey data available. The data helped us build uni-gram, bi-gram and tri-gram datasets. These are used for predicting the next word.
 
 #### Steps
 1. Upon initial load up of the application, **"NULL"** will be displayed at the Predicted Next Word textbox.
 2. User may then enter a partially complete sentence to begin the next word prediction.
-
 
 #### Results
 1. Next word predicted is shown at the Predicted Next Word textbox.
 2. Information on the n-gram backoff algorithm used to predict is shown as indication.
 
 #### Note:
-The source codes for ui.R and server.R files are also available on the [GitHub repo](https://github.com/sachinvraje/capstone)
-
-Presentation Slide decks are available on the [RPubs page](http://rpubs.com/sachinvraje/capstone)
+1. The source codes for ui.R, server.R and other files are available on [GitHub repo](https://github.com/sachinvraje/capstone)
+2. Presentation Slide decks are available on the [RPubs page](http://rpubs.com/sachinvraje/capstone)
+3. Working application can be accessed via [Shiny App](https://sachinvraje.shinyapps.io/capstone/)
 
 Thank You
+
 Regards
 
-####Sachin Raje
+#### Sachin Raje
--- ui.R
+++ ui.R
@@ -10,14 +10,14 @@ shinyUI(navbarPage("Coursera Data Science Capstone: Course Project",
                             br(),
                             HTML("<strong>Date: 12 Feb 2017</strong>"),
                             br(),
-                            img(src = "./headers.png"),
+#                            img(src = "/Users/Sachin/OneDrive%20-%20emiratesgroup/coursera/capstone/headers2.png"),
                             # Sidebar
                             sidebarLayout(
                                sidebarPanel(
 #                                  helpText("Enter a partially complete sentence to begin the next word prediction"),
-                                  textInput("inputString", "Enter a partial sentence here to predict next word",value = ""),
-                                  br(),
-                                  strong("Only English words are supported."),
+                                  textInput("inputString", h4("Enter a partial sentence to predict next word"),value = ""),
+#                                  br(),
+                                  h4("Only English words are supported."),
                                   br(),
                                   br(),
                                   br()
@@ -26,11 +26,11 @@ shinyUI(navbarPage("Coursera Data Science Capstone: Course Project",
                                   h2("Predicted Next Word"),
                                   verbatimTextOutput("prediction"),
                                   strong("Sentence Input:"),
-                                  tags$style(type='text/css', '#text1 {background-color: rgba(255,255,0,0.40); color: blue;}'), 
+                                  tags$style(type='text/css', '#text1 {background-color: rgba(255,128,128,0.40); color: blue;}'), 
                                   textOutput('text1'),
                                   br(),
                                   strong("Note:"),
-                                  tags$style(type='text/css', '#text2 {background-color: rgba(255,255,0,0.40); color: black;}'),
+                                  tags$style(type='text/css', '#text2 {background-color: rgba(255,128,128,0.40); color: black;}'),
                                   textOutput('text2')
                                )
                             )
@@ -38,7 +38,7 @@ shinyUI(navbarPage("Coursera Data Science Capstone: Course Project",
                    ),
                    tabPanel("About",
                             mainPanel(
-                               img(src = "./headers.png"),
+#                               img(src = "./headers.png"),
                                includeMarkdown("./about.md")
                             )
                    )
